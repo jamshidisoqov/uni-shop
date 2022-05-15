4e5be7d50652e6ca974cfddf6a05d700da20734c
jamshidisoqov <isoqovjamshid01@gmail.com>
2022-05-15 22:30:52 +0500

    models changed

--- app/src/main/java/io/jamshid/unishop/common/Result.kt
+++ app/src/main/java/io/jamshid/unishop/common/Result.kt
@@ -1,5 +1,5 @@
 package io.jamshid.unishop.common
 
-// Created by Usmon Abdurakhmanv on 5/13/2022.
+// Created by Usmon Abdurakhmanov on 5/13/2022.
 
 data class Result<T>(var message: String, var active: Boolean, val status: Int, var body: T)
--- app/src/main/java/io/jamshid/unishop/data/models/dto/OutputSales.kt
+++ app/src/main/java/io/jamshid/unishop/data/models/dto/OutputSales.kt
@@ -1,18 +1,18 @@
 package io.jamshid.unishop.data.models.dto
 
+import java.sql.Timestamp
 import java.util.*
 
 // Created by Jamshid Isoqov an 5/15/2022
 data class OutputSales(
     val id: Long,
     val client: Client,
-    val products: Set<OutputProduct>,
-    val paymentHistories: Set<PaymentHistory>,
     val status: PaymentStatus,
     val debtAmount: Double,
     val expiredDate: Date,
     val comment: String,
-    val createdDate: Date,
-    val updatedDate: Date,
+    val amount:Double,
+    val createdDate: Timestamp,
+    val updatedDate: Timestamp,
     val createdBy: UUID
 )
